b396665113ca828b3e89f6bda53dce716b18eacd
Tribhuwan Sori <137310764+Tribhuone@users.noreply.github.com>
2025-08-30 02:26:24 +0530

    Update Home.jsx

--- clientSide/src/pages/Home.jsx
+++ clientSide/src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import "../App.css";
 import axios from 'axios';
 import { useState , useEffect } from "react";
-
+import SkeletonCard from "../components/ImgCardLoader";
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import ImageCard from '../components/ImgCard';
@@ -11,14 +11,17 @@ import Footer from "../components/Footer";
 const Home = () => {
 
     const [items, setItems] = useState([]);
-
+    const [loading, setLoading] = useState(true);
+    
     useEffect( () => {
         axios.get(`${import.meta.env.VITE_PRODUCT_SERVICE_URL}/api/product`)
         .then( (res) => {
             setItems(res.data);
+            setLoading(false);
         })
         .catch( (er) => {
             console.log(er.response.message);
+            setLoading(false);
         });
     }, []);
 
@@ -29,7 +32,12 @@ const Home = () => {
                 <main className=" flex-grow p-4 max-sm:px-0">
                     <Hero/>
                     <div className="flex justify-evenly items-center flex-wrap xl:px-20 2xl:px-20 md: max-sm:px-0 all-Img-Cards drop-shadow-md" >
-                        <ImageCard items={items}/>
+                        {loading
+                            ? // show 4 skeletons while loading
+                              Array.from({ length: 4 }).map((_, idx) => <SkeletonCard key={idx} />)
+                            : // show real images
+                                <ImageCard items={items}/>
+                        }
                     </div>
                 </main>
                 <Footer/>
