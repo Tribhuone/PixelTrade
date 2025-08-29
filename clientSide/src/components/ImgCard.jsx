bfe74b7cc3c6e5f976d4b1dc8ac1b01045ea4ce0
Tribhuwan Sori <137310764+Tribhuone@users.noreply.github.com>
2025-08-30 03:33:08 +0530

    Update ImgCard.jsx

--- clientSide/src/components/ImgCard.jsx
+++ clientSide/src/components/ImgCard.jsx
@@ -44,8 +44,7 @@ const ImageCard = ({ items }) => {
         else{
             <Link to={`/img/${item._id}`} />
             dispatch(updateImgId(item._id));
-            dispatch(updateImgPath(item.path));
-            navigate(`/img/${item._id}`, { state : item } );
+            navigate(`/img/${item._id}`);
         }
     }
 
