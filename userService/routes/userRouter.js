533828464f3bdbf67da71a78c3302d9cdda7308b
Tribhuwan Sori <137310764+Tribhuone@users.noreply.github.com>
2026-01-26 17:24:48 +0530

    Add updateUserProfile route to userRouter

--- userService/routes/userRouter.js
+++ userService/routes/userRouter.js
@@ -2,7 +2,12 @@
 
 const express = require("express");
 
-const { register , verifyOTP , userLogin , userLogOut , getUser , forgotPassword , resetPassword } = require("../controllers/userController.js");
+const { register , verifyOTP , 
+        userLogin , userLogOut , 
+        getUser , forgotPassword , 
+        resetPassword, updateUserProfile 
+    } = require("../controllers/userController.js");
+
 const { isAuthenticate } = require("../middlewares/auth.js");
 
 const router = express.Router();
@@ -14,5 +19,6 @@ router.get("/logout" , userLogOut);
 router.get("/:id", isAuthenticate , getUser);
 router.post("/password/forgot", forgotPassword);
 router.put("/password/reset/:token", resetPassword);
+router.patch("/update-profile", updateUserProfile);
 
 module.exports = router;
