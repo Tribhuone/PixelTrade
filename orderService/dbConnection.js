32e5715a7def3a440fbdc13dde995417fd5073c0
Tribhuwan Sori <137310764+Tribhuone@users.noreply.github.com>
2026-02-03 03:05:02 +0530

    Refactor database connection to async/await
    Refactor database connection to use async/await and improved error handling.

--- orderService/dbConnection.js
+++ orderService/dbConnection.js
@@ -1,14 +1,20 @@
 
 const mongoose = require("mongoose");
 
-const databaseConnection = () => {
-    mongoose.connect(process.env.MONGO_URL)
-    .then( () => {
-        console.log("Database connected");
-    })
-    .catch( (err) => {
-        console.log(err);
+const databaseConnection = async () => {
+  try {
+    mongoose.set("bufferCommands", false); // 🔥 important
+
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 5000,
     });
-}
+
+    console.log("MongoDB Connected");
+
+  } catch (err) {
+    console.log("Mongo Connection Error:", err);
+    process.exit(1);
+  }
+};
 
 module.exports = databaseConnection;
